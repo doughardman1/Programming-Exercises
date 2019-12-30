897f42e4072543f71e7935ce44675ac0ae337b93
douglashardman1 <douglas@fiveelms.co.uk>
2019-12-30 23:51:04 +0000

    adding bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+# Bubble sort
+
+def bubble_sort(numbers):
+    swapped = True
+    
+    while swapped:
+        swapped = False
+        for i in range(len(numbers) - 1):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i] #swap numbers
+                swapped = True
+    return numbers
+
+numbers = [2,5,7,2,567,2435,6]
+
+print(bubble_sort(numbers))
